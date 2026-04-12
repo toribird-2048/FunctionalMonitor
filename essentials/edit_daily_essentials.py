6d61ab5f5dbd2d5277d3bcf1bad3076f8ac656e4
fly-toribird <166600296+toribird-2048@users.noreply.github.com>
2026-04-12 04:24:23 +0000

    feat: 教科ごとに持ち物を管理するように変更

--- essentials/edit_daily_essentials.py
+++ essentials/edit_daily_essentials.py
@@ -3,29 +3,29 @@ from questionary import Choice
 import json
 import sys
 
-items_path = "essentials/items.json"
-essential_items_path = "essentials/daily_essentials.json"
-items = []
+subjects_items_kv_path = "essentials/items.json"
+essential_items_path = "essentials/timetable.json"
+daily_essentials_path = "essentials/daily_essentials.json"
+subjects_items_kv:dict[str, list[str]] = {}
 
 try:
-    with open(items_path, "r", encoding="utf-8") as f:
+    with open(subjects_items_kv_path, "r", encoding="utf-8") as f:
         content = json.load(f)
-        if isinstance(content, list):
-            items = content
+        if isinstance(content, dict):
+            subjects_items_kv = content
         else:
-            print("Error: items.json must be a list.")
+            print("Error: items.json must be a dict.")
             sys.exit(1)
 except FileNotFoundError:
-    print(f"Error: {items_path} not found.")
+    print(f"Error: {subjects_items_kv_path} not found.")
     sys.exit(1)
 
-daily_essentials = {}
+timetable = {}
 try:
     with open(essential_items_path, "r", encoding="utf-8") as f:
-        essential_content = json.load(f)
-        print(content)
-        if isinstance(essential_content, dict):
-            daily_essentials = essential_content
+        timetable_content = json.load(f)
+        if isinstance(timetable_content, dict):
+            timetable = timetable_content
         else:
             print("Error: daily_essentials.json must be a dict.")
             sys.exit(1)
@@ -36,18 +36,19 @@ except Exception as e:
 
 days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-selected_items = {}
+selected_subjects = {}
+daily_essentials = {}
 
-items_set = set(items)
-daily_essentials_set = {k: set(v) for k,v in daily_essentials.items()}
+subjects_set = set(subjects_items_kv.keys())
+timetable_set = {k: set(v) for k,v in timetable.items()}
 
 for day in days:
-    if daily_essentials_set[day] == set([]):
-        options = ["new_item"]
+    if timetable_set[day] == set([]):
+        options = ["new_subject"]
     else:
         options = choices=[
-            *map(lambda x : Choice(x, checked=True), daily_essentials_set[day]),
-            Choice("new_item", checked=False)
+            *map(lambda x : Choice(x, checked=True), timetable_set[day]),
+            Choice("new_subject", checked=False)
         ]
     res = set(questionary.checkbox(
         f"Select essentials ({day})",
@@ -56,22 +57,30 @@ for day in days:
     if res is None:
         print("\nInterrupted by user.")
         sys.exit(0)
-    if "new_item" in res:
+    if "new_subject" in res:
         res = res.union(set(questionary.checkbox(
             f"Select new item ({day})",
-            choices=items_set.difference(daily_essentials_set[day])
+            choices=subjects_set.difference(timetable_set[day])
         ).ask()))
-    res.discard("new_item")
-    selected_items[day] = list(res)
-    print(res)
+    res.discard("new_subject")
+    selected_subjects[day] = list(res)
+    print(selected_subjects[day])
+    daily_essentials[day] = list(set([]).union(*[set(subjects_items_kv[subject]) for subject in selected_subjects[day]]))
 
 if questionary.confirm("Save this configuration?").ask():
     try:
         with open(essential_items_path, "w", encoding="utf-8") as f:
-            json.dump(selected_items, f, ensure_ascii=False, indent=4)
+            json.dump(selected_subjects, f, ensure_ascii=False, indent=4)
         print(f"Successfully saved to {essential_items_path}")
     except Exception as e:
         print(f"Failed to edit {essential_items_path}")
+        print(str(e))
+    try:
+        with open(daily_essentials_path, "w", encoding="utf-8") as f:
+            json.dump(daily_essentials, f, ensure_ascii=False, indent=4)
+        print(f"Successfully saved to {daily_essentials_path}")
+    except Exception as e:
+        print(f"Failed to edit {daily_essentials_path}")
         print(str(e))
 else:
     print("Discarded changes.")
