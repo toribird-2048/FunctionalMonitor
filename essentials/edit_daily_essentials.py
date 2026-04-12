78896a6de6bba58492e59d6d02e1d9f0ca8314b0
fly-toribird <166600296+toribird-2048@users.noreply.github.com>
2026-04-12 03:43:43 +0000

    feat: edit_daily_essential.pyを、daily_essential.jsonを新しく上書きするのではなく、編集で(追加|削除)する形に変えた。

--- essentials/edit_daily_essentials.py
+++ essentials/edit_daily_essentials.py
@@ -1,4 +1,5 @@
 import questionary
+from questionary import Choice
 import json
 import sys
 
@@ -18,20 +19,51 @@ except FileNotFoundError:
     print(f"Error: {items_path} not found.")
     sys.exit(1)
 
+daily_essentials = {}
+try:
+    with open(essential_items_path, "r", encoding="utf-8") as f:
+        essential_content = json.load(f)
+        print(content)
+        if isinstance(essential_content, dict):
+            daily_essentials = essential_content
+        else:
+            print("Error: daily_essentials.json must be a dict.")
+            sys.exit(1)
+except Exception as e:
+    print(f"Failed to read {essential_items_path}")
+    print(str(e))
+    sys.exit(1)
+
 days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 selected_items = {}
 
-for day in days:
-    res = questionary.checkbox(
-        f"Select essentials ({day})",
-        choices=items
-    ).ask()
+items_set = set(items)
+daily_essentials_set = {k: set(v) for k,v in daily_essentials.items()}
 
+for day in days:
+    if daily_essentials_set[day] == set([]):
+        options = ["new_item"]
+    else:
+        options = choices=[
+            *map(lambda x : Choice(x, checked=True), daily_essentials_set[day]),
+            Choice("new_item", checked=False)
+        ]
+    res = set(questionary.checkbox(
+        f"Select essentials ({day})",
+        choices=options
+    ).ask())
     if res is None:
         print("\nInterrupted by user.")
         sys.exit(0)
-    selected_items[day] = res
+    if "new_item" in res:
+        res = res.union(set(questionary.checkbox(
+            f"Select new item ({day})",
+            choices=items_set.difference(daily_essentials_set[day])
+        ).ask()))
+    res.discard("new_item")
+    selected_items[day] = list(res)
+    print(res)
 
 if questionary.confirm("Save this configuration?").ask():
     try:
